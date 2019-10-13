42a192aad3910d86133bc6c59bc40954eebeaf58
Josh Newton <josh-newton@hotmail.co.uk>
2019-10-13 11:38:08 +0100

    Initial Commit

---
+++ src/App/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    const {} = this.state;
+
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default App;
